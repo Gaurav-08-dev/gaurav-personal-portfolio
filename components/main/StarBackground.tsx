0c2344887a57f6ed982c52c554df4f948538e568
gaurav <g8111997@gmail.com>
2024-01-24 23:55:48 +0530

    Implement Hero Section - Done

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react'
+import { useState, useRef, Suspense } from 'react';
+import {Canvas, useFrame} from "@react-three/fiber";
+import {Points, PointMaterial, Preload} from "@react-three/drei";
+
+const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,8 +2,11 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromTop } from "@/utils/motion";
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+
 const HeroContent = () => {
   return (
     <motion.div
@@ -19,8 +22,46 @@ const HeroContent = () => {
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">Frontend Developer Portfolio</h1>
         </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Hey!
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              I&apos;m Gaurav{" "}
+            </span>
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a Frontend Engineer with experience in building webapps and currently learning MERN stack Check out my projects and skills
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
       </div>
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
     </motion.div>
+
   );
 };
 
