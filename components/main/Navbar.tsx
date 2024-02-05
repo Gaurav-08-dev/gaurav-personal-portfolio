2ecf1a890c86a134e089f1f144e97e55992e7e95
gaurav <g8111997@gmail.com>
2024-02-06 01:40:21 +0530

    Complete navbar design - done

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { Socials } from "@/constants";
 
 const Navbar = () => {
   return (
@@ -10,22 +11,54 @@ const Navbar = () => {
     >
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
-          href="about-me"
+          href="#about-me"
           className="h-auto w-auto
         flex flex-row items-center
         "
         >
           <Image
-            src="/Navlogo.png"
+            src="/glaxy.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={70}
+            height={70}
             className="cursor-pointer hover:animate-slowspin"
           />
           {/* <span className="font-bold ml-[10px] hidden md:block text-gray-300">
             Gaurav Singh
           </span> */}
         </a>
+
+        <div
+          className="w-[500px] h-full flex 
+        flex-row items-center justify-between md:mr-20"
+        >
+          <div
+            className="flex items-center justify-between w-full h-auto border border-[#7842f861] bg=[#0300145e] mr-[15px] px-[20px]
+            py-[10px] rounded-full text-gray-200
+            "
+          >
+            <a href="#about-me" className="cursor-pointer">
+              About me
+            </a>
+            <a href="#skills" className="cursor-pointer">
+              Skills
+            </a>
+            <a href="#projects" className="cursor-pointer">
+              Projects
+            </a>
+          </div>
+        </div>
+          <div className="flex flex-row gap-5">
+            {Socials.map((social) => (
+              <Image
+                src={social.src}
+                alt={social.name}
+                key={social.name}
+                width={24}
+                height={24}
+              />
+            ))}
+          </div>
       </div>
     </div>
   );
