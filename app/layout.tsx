502193b87ea2dcf68ca9ad842cb017b9ea79646e
gaurav <g8111997@gmail.com>
2024-02-05 23:51:18 +0530

    Add Navbar logo - Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Gaurav - Frontend Developer",
   description: "",
-  icons:"/public/glaxy.png"
+  // icons:"/public/glaxy.png"
 };
 
 export default function RootLayout({
@@ -22,6 +23,7 @@ export default function RootLayout({
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
         <StarCanvas />
+        <Navbar />
         {children}
       </body>
     </html>
