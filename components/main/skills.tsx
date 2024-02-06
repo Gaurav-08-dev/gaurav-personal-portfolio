5102bbbf9288b8259262ba38738b23ba0aa4bd86
gaurav <g8111997@gmail.com>
2024-02-06 23:11:58 +0530

    Update skillset- Done

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -7,14 +7,12 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 relative overflow-hidden py-20"
+      className="flex flex-col items-center justify-center gap-3 relative overflow-hidden pb-80 py-20"
       style={{ transform: "scale(0.9)" }}
     >
       <SkillText />
       <div
-        className="flex 
-      flex-row justify-around 
-      flex-wrap mt-4 gap-5 items-center"
+        className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center"
       >
         {Frontend_skill.map((image, index) => (
           <SkillDataProvider
