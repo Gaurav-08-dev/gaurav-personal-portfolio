7adb428be6959b3139bb1704380241e182ef0ae1
gaurav <g8111997@gmail.com>
2024-02-06 23:59:44 +0530

    Update learn more button - Done

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -45,6 +45,8 @@ const HeroContent = () => {
         <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          href="https://github.com/Gaurav-08-dev"
+          target="_blank"
         >
           Learn More!
         </motion.a>
