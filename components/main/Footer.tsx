752406aa188c8d5c0fffbe2dd122a706cdf59824
gaurav <g8111997@gmail.com>
2024-02-09 03:36:04 +0530

    fixed footer style

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
-       <div className="mb-[20px] text-[15px] text-center">
+       <div className=" text-[15px] text-center">
                 &copy; Gaurav 2024 Inc. All rights reserved
             </div>
     </div>
